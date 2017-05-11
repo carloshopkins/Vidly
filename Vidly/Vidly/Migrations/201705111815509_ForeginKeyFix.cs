7a8dd6c9278e2d09b6e29e71af22cdf99897e16c
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-11 14:52:19 -0400

    Populated database

---
+++ Vidly/Vidly/Migrations/201705111815509_ForeginKeyFix.cs
@@ -0,0 +1,16 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ForeginKeyFix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
